cab49190b8623650cb63b18ce598d3c1db9f3b03
Coerulatus <marcomontagna21@gmail.com>
2025-03-08 18:18:50 +0000

    tested on cora

--- topobench/transforms/liftings/graph2simplicial/dnd_lifting.py
+++ topobench/transforms/liftings/graph2simplicial/dnd_lifting.py
@@ -24,13 +24,15 @@ class SimplicialDnDLifting(Graph2SimplicialLifting):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def lift_topology(self, data: Data) -> dict:
+    def lift_topology(self, data: Data, max_simplices=100) -> dict:
         r"""Lift the topology of a graph to a simplicial complex using Dungeons & Dragons (D&D) inspired mechanics.
 
         Parameters
         ----------
         data : Data
             The input data to be lifted.
+        max_simplices : int
+            Maximum number of simplices to add for each node attribute.
 
         Returns
         -------
@@ -41,22 +43,29 @@ class SimplicialDnDLifting(Graph2SimplicialLifting):
         simplicial_complex = SimplicialComplex()
 
         characters = self._assign_attributes(graph)
-        simplices = [set() for _ in range(2, self.complex_dim + 1)]
+        simplices = [set() for _ in range(1, self.complex_dim + 1)]
 
         for node in graph.nodes:
             simplicial_complex.add_node(node, features=data.x[node])
 
         for node in graph.nodes:
             character = characters[node]
-            for k in range(1, self.complex_dim):
+            for k in range(1, self.complex_dim + 1):
                 dice_roll = self._roll_dice(character, k)
                 neighborhood = list(
                     nx.single_source_shortest_path_length(
                         graph, node, cutoff=dice_roll
                     ).keys()
                 )
-                for combination in combinations(neighborhood, k + 1):
+                neighborhood = neighborhood[1:]
+                random.shuffle(neighborhood)
+                neighborhood = [node, *neighborhood]
+                for i, combination in enumerate(
+                    combinations(neighborhood, k + 1)
+                ):
                     simplices[k - 1].add(tuple(sorted(combination)))
+                    if i >= max_simplices:
+                        break
 
         for set_k_simplices in simplices:
             simplicial_complex.add_simplices_from(list(set_k_simplices))
@@ -78,9 +87,9 @@ class SimplicialDnDLifting(Graph2SimplicialLifting):
         """
         degrees = nx.degree_centrality(graph)
         clustering = nx.clustering(graph)
-        closeness = nx.closeness_centrality(graph)
+        # closeness = nx.closeness_centrality(graph)
         eigenvector = nx.eigenvector_centrality(graph, tol=1e-3)
-        betweenness = nx.betweenness_centrality(graph)
+        # betweenness = nx.betweenness_centrality(graph)
         pagerank = nx.pagerank(graph)
 
         attributes = {}
@@ -88,9 +97,9 @@ class SimplicialDnDLifting(Graph2SimplicialLifting):
             attributes[node] = {
                 "Degree": degrees[node],
                 "Clustering": clustering[node],
-                "Closeness": closeness[node],
+                # "Closeness": closeness[node],
                 "Eigenvector": eigenvector[node],
-                "Betweenness": betweenness[node],
+                # "Betweenness": betweenness[node],
                 "Pagerank": pagerank[node],
             }
         return attributes
@@ -116,15 +125,17 @@ class SimplicialDnDLifting(Graph2SimplicialLifting):
             attribute = attributes["Degree"]
         elif k == 2:
             attribute = attributes["Clustering"]
+        # elif k == 3:
+        #     attribute = attributes["Closeness"]
         elif k == 3:
-            attribute = attributes["Closeness"]
-        elif k == 4:
             attribute = attributes["Eigenvector"]
-        elif k == 5:
-            attribute = attributes["Betweenness"]
+        # elif k == 5:
+        #     attribute = attributes["Betweenness"]
         else:
             attribute = attributes["Pagerank"]
 
         base_roll = random.randint(1, 20)
         modifier = int(attribute * 20)
-        return base_roll + modifier
+        return (
+            base_roll + modifier
+        ) / 10  # 4 seems like a reasonable max number of hops to consider
