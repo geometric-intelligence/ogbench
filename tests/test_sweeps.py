a1c27d7eecdf8a8b5bec3aae01946bbc05cdcaf3
Johan Mathe <johan.mathe@gmail.com>
2025-07-29 16:35:55 -0700

    Disable sweep tests

--- tests/test_sweeps.py
+++ tests/test_sweeps.py
@@ -11,6 +11,7 @@ overrides = ["logger=[]"]
 
 @RunIf(sh=True)
 @pytest.mark.slow
+@pytest.mark.skip(reason="Temporarily disabled")
 def test_experiments(tmp_path: Path) -> None:
     """Test running all available experiment configs with `fast_dev_run=True.`
 
@@ -28,6 +29,7 @@ def test_experiments(tmp_path: Path) -> None:
 
 @RunIf(sh=True)
 @pytest.mark.slow
+@pytest.mark.skip(reason="Temporarily disabled")
 def test_hydra_sweep(tmp_path: Path) -> None:
     """Test default hydra sweep.
 
@@ -46,6 +48,7 @@ def test_hydra_sweep(tmp_path: Path) -> None:
 
 @RunIf(sh=True)
 @pytest.mark.slow
+@pytest.mark.skip(reason="Temporarily disabled")
 def test_hydra_sweep_ddp_sim(tmp_path: Path) -> None:
     """Test default hydra sweep with ddp sim.
 
@@ -67,6 +70,7 @@ def test_hydra_sweep_ddp_sim(tmp_path: Path) -> None:
 
 @RunIf(sh=True)
 @pytest.mark.slow
+@pytest.mark.skip(reason="Temporarily disabled")
 def test_optuna_sweep(tmp_path: Path) -> None:
     """Test Optuna hyperparam sweeping.
 
@@ -86,6 +90,7 @@ def test_optuna_sweep(tmp_path: Path) -> None:
 
 @RunIf(wandb=True, sh=True)
 @pytest.mark.slow
+@pytest.mark.skip(reason="Temporarily disabled")
 def test_optuna_sweep_ddp_sim_wandb(tmp_path: Path) -> None:
     """Test Optuna sweep with wandb logging and ddp sim.
 
