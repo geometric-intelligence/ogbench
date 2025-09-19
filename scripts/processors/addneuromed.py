8897d13735058b426f60eee7da09117c0929fcb5
Johan Mathe <johan.mathe@gmail.com>
2025-09-19 10:37:38 -0700

    Update addneuromed

--- scripts/processors/addneuromed.py
+++ scripts/processors/addneuromed.py
@@ -212,8 +212,11 @@ def process_addneuromed(output_dir: str = "temp_data") -> None:
     raw_data = pd.concat(frames, axis=0)
     targets = np.array(statuses)
 
-    # Remove rows with nan values
-    mask = (~pd.isna(raw_data)).any(axis=1).values & ~pd.isna(targets)
+    # Define classes to remove
+    classes_to_remove = {"CTL to AD", "MCI to CTL", "OTHER", "borderline MCI"}
+    
+    # Filter out unwanted classes
+    mask = (~pd.isna(raw_data)).any(axis=1).values & ~pd.isna(targets) & ~np.isin(targets, list(classes_to_remove))
     raw_data = raw_data[mask]
     targets = targets[mask]
 
